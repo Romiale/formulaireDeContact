6f769f6e0f1f5f0bff020837b945e54958b8ec62
Romiale <59141618+Romiale@users.noreply.github.com>
2021-04-12 12:36:08 +0200

    update hidden

--- formulaire.js
+++ formulaire.js
@@ -33,7 +33,8 @@ onClickperson = event => {
         var new_bdge = document.createElement("div")
         var boutonSupprimer = document.createElement("button");
         var spanId = document.createElement('span');
-        spanId.innerText = element.id.classeName="hidden";
+        spanId.innerText = element.id;
+        spanId.className="hidden";
         boutonSupprimer.innerText = spanId.innerText + " X";
         var prenom = document.createElement("h5");
         prenom.innerText = element.prenom + ' ' + element.nom + '   ' + '                 id: ' + spanId.innerText;
